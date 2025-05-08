8e4c257df55053efc8b15c0918934973662006fc
mihaisacalianu <mihaisacalianu@gmail.com>
2025-05-08 12:02:53 +0100

    03-created five components pages

---
+++ frontend/src/pages/EditEventPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditEventPage() {
+  return (
+    <div>
+      <h1>Edit Event page</h1>
+    </div>
+  )
+}
+
+export default EditEventPage
---
+++ frontend/src/pages/NewEventPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewEventPage() {
+  return (
+    <div>
+      <h1>New Event Page</h1>
+    </div>
+  )
+}
+
+export default NewEventPage
